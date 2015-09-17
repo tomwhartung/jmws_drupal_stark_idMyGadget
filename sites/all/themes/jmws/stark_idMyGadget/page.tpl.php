82243fc636e4daed1dd10d50be35f49600639ca7
Tom W Hartung <tomwhartung@gmail.com>
2015-09-17 13:09:05 -0600

    Added some "Hello"s, they are not appearing, more study is needed, obviously.

--- sites/all/themes/jmws/stark_idMyGadget/page.tpl.php
+++ sites/all/themes/jmws/stark_idMyGadget/page.tpl.php
@@ -4,22 +4,29 @@
  *   http://www.apaddedcell.com/how-create-drupal-7-theme-scratch
  */
 <div id="header">
- 
+  <p>Hello Header</p>
 </div>
 
 <div id="wrapper">
 
+  <p>Hello Wrapper</p>
+
   <div id="content">
+
+    <p>Hello Content</p>
 
   </div>
     
   <div id="sidebar">
+
+    <p>Hello Sidebar</p>
 
   </div>
 
 </div>
 
 <div id="footer">
+  <p>Hello Footer</p>
 
 </div>
 
