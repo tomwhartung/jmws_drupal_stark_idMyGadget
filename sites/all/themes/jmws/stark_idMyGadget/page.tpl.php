5ce2ec42f4afdf122f32a65ae833048a931f9806
Tom W Hartung <tomwhartung@gmail.com>
2015-09-17 14:03:13 -0600

    Just experimenting, and finding it very interesting.

--- sites/all/themes/jmws/stark_idMyGadget/page.tpl.php
+++ sites/all/themes/jmws/stark_idMyGadget/page.tpl.php
@@ -3,30 +3,51 @@
  * Experimenting for now, inspired by this page:
  *   http://www.apaddedcell.com/how-create-drupal-7-theme-scratch
  */
+?>
+
 <div id="header">
-  <p>Hello Header</p>
+
+  <?php if ($page['header']): ?>    
+    <?php print render($page['header']); ?>
+  <?php else : ?>
+    <p>Hello header is here because we have no header!</p>
+  <?php endif; ?>
+
 </div>
 
 <div id="wrapper">
 
-  <p>Hello Wrapper</p>
+  <p>Hello Wrapper!  Can we "have" a "wrapper?"</p>
 
   <div id="content">
 
-    <p>Hello Content</p>
+    <?php if ($page['content']): ?>    
+      <?php print render($page['content']); ?>
+    <?php else : ?>
+      <p>Hello content is here because we have no content!</p>
+    <?php endif; ?>
 
   </div>
     
   <div id="sidebar">
 
-    <p>Hello Sidebar</p>
+    <?php if ($page['sidebar']): ?>    
+      <?php print render($page['sidebar']); ?>
+    <?php else : ?>
+      <p>Hello sidebar is here because we have no sidebar!</p>
+    <?php endif; ?>
 
   </div>
 
 </div>
 
 <div id="footer">
-  <p>Hello Footer</p>
+
+  <?php if ($page['footer']): ?>    
+    <?php print render($page['footer']); ?>
+  <?php else : ?>
+    <p>Hello footer is here because we have no footer!</p>
+    <?php endif; ?>
 
 </div>
 
