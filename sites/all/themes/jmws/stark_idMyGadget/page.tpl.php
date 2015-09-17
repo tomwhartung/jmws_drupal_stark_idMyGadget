4fd7402a3b3634b5f618305b1b644c1c9c5ac894
Tom W Hartung <tomwhartung@gmail.com>
2015-09-17 12:48:22 -0600

    Adding an experimental page.tpl.php based on the post mentioned in the comments.

---
+++ sites/all/themes/jmws/stark_idMyGadget/page.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Experimenting for now, inspired by this page:
+ *   http://www.apaddedcell.com/how-create-drupal-7-theme-scratch
+ */
+<div id="header">
+ 
+</div>
+
+<div id="wrapper">
+
+  <div id="content">
+
+  </div>
+    
+  <div id="sidebar">
+
+  </div>
+
+</div>
+
+<div id="footer">
+
+</div>
+
