9037cd25c53763935421eba02fc502b48adfd440
Tom W Hartung <tomwhartung@gmail.com>
2015-09-20 19:43:59 -0600

    Refined my error messages.  Nice.

--- sites/all/themes/jmws/stark_idMyGadget/idMyGadget/JmwsIdMyGadgetNoDetection.php
+++ sites/all/themes/jmws/stark_idMyGadget/idMyGadget/JmwsIdMyGadgetNoDetection.php
@@ -19,13 +19,13 @@ define( 'IDMYGADGET_ERROR_PROLOG',
  * Error message for when the plugin is not installed
  */
 define( 'IDMYGADGET_NOT_INSTALLED',
-	IDMYGADGET_ERROR_PROLOG .
+	IDMYGADGET_ERROR_PROLOG . '<p>It appears this module is not installed.</p>' .
 	'<p>Please <span class="idmygadget-error">install and activate the module,</span> which is available on github, or use a different theme.</p></div>' );
 /**
  * Error message for when the plugin is not active
  */
 define( 'IDMYGADGET_NOT_ACTIVE',
-	IDMYGADGET_ERROR_PROLOG .
+	IDMYGADGET_ERROR_PROLOG . '<p>It appears this module is installed but not active.</p>' .
 	'<p>Please <span class="idmygadget-error">activate the module</span> in the Drupal administration console, or use a different theme.</p></div>' );
 /**
  * Error message for when there is an unknown error (bug?)
