f05b3eb14d279c565e7fe9262dda311eee7d3f8f
Tom W Hartung <tomwhartung@gmail.com>
2015-10-16 10:20:54 -0600

    Updated doctype and html tags in html.tpl.php for html5.

--- sites/all/themes/jmws/stark_idMyGadget/html.tpl.php
+++ sites/all/themes/jmws/stark_idMyGadget/html.tpl.php
@@ -42,13 +42,12 @@
  *
  * @ingroup themeable
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+?><!DOCTYPE html>
+<html>
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
+  <title>stark:<?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
